7a8930976618cfa3f05bc6f8168b11105183c8a0
YAhiroya <32817862+YAhiroya@users.noreply.github.com>
2018-11-23 14:08:16 +0900

    まだまだだな！

--- 学部生開発/ClusterIncorporation/server.R
+++ 学部生開発/ClusterIncorporation/server.R
@@ -1,33 +1,16 @@
+library(tidyverse) # この中にggplot2,readr,dplyrが含まれている
 library(tcltk)
 library(devtools)
 library(ggTimeSeries)
-library(ggplot2)
 library(readr)
 library(shiny)
-library(dplyr)
-library(rgdal)
-library(RColorBrewer)
-library(googleVis)
-library(leaflet)
 library(DT)
 library(knitr)
 library(rmarkdown)
 library(shinyBS)
+library(plyr)
+######## パッケージが重複することの無いように！！！！！ #################################
 
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
-library(tcltk)
-library(devtools)
-library(ggTimeSeries)
-library(ggplot2)
-library(readr)
 #max(Dataset$X3)   #欠損値があるかどうか確認できる
 #Dataset[2]<-lapply(Dataset[2],abs)　#マイナスの欠損値を絶対値でプラスに変更する
 #Dataset[which(Dataset$X8 > 4000),8] 
@@ -60,9 +43,6 @@ kmean.y <- kmeans(y, inital.v[2:7,])
 
 
 #カレンダープロットの作成
-library(plyr)
-library(ggplot2)
-
 cal.mat <- as.data.frame(cbind("Date" = date.day, "Cluster" = kmean.y$cluster))
 
 cal.mat[,1] <- as.Date(cal.mat[,1])
@@ -78,25 +58,11 @@ xxx <- ddply(xxx, .(Month), transform, WeekN = 1 + week - min(week))
 pp <- ggplot(xxx, aes(Week, WeekN, fill = Clust))
 ppp   <- pp + geom_tile(color="gray")+   facet_wrap(~ Month, ncol = 2, dir = "v") + scale_y_reverse()
 
-output$RenderPlot
-library(shiny)
+output$RenderPlot　# ここに記述しても意味がない！Shinyサーバーの中に入れるように！！！！
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-  
-  output$distPlot <- renderPlot({
-  w <- ppp[,2] bins <- seq(min(x),max(x),length.out = input$bins+1)
-  hist(x,breaks = bins,col = 'darkgray',border = 'white')
-  
-  
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+# Shinyサーバー
+shinyServer(function(input, output, session) {
+  output$RenderPlot <- renderPlot({
+    print(ppp)
   })
-  
 })
